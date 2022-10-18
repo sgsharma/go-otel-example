91e373bcffa32e10d568d1118510f16bf7332078
sgsharma <sashasharma@honeycomb.io>
2022-10-18 12:12:07 -0700

    update for demo

--- go.mod
+++ go.mod
@@ -31,10 +31,12 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.10.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.10.0 // indirect
@@ -42,7 +44,7 @@ require (
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
 	golang.org/x/net v0.0.0-20220926192436-02166a98028e // indirect
-	golang.org/x/sys v0.0.0-20220926163933-8cfa568d3c25 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220926220553-6981cbe3cfce // indirect
 	google.golang.org/grpc v1.49.0 // indirect
--- main.go
+++ main.go
@@ -4,93 +4,95 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"runtime/debug"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	// instrument the gin-gonic web server
+	// auto-instrument the gin-gonic web server
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	// instrument HTTP requests
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
-	"go.opentelemetry.io/otel/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 	_ "go.uber.org/automaxprocs"
 )
 
 func initTracer() func() {
 
+	// Top-level Context for incoming requests
 	ctx := context.Background()
 
+	// Create trace exporter to be able to retrieve
+	// the collected spans.
 	exporter, err := otlptracegrpc.New(ctx)
 	if err != nil {
-		log.Fatal(err)
+		log.Ctx(ctx).Err(err).Msg("Err in creating otlp exporter")
 	}
+
+	// Register the exporter with the TracerProvider using
+	// a BatchSpanProcessor
 	provider := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(exporter),
-		sdktrace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String("ExampleService"))),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("go-example"),
+		)),
 	)
 	otel.SetTracerProvider(provider)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 	return func() {
 		ctx := context.Background()
+		// Shutdown will flush any remaining spans and shut down the exporter
 		err := provider.Shutdown(ctx)
 		if err != nil {
-			log.Fatal(err)
+			log.Ctx(ctx).Err(err).Msg("Err in otlp shutdown function")
 		}
 	}
 }
 
-type temp struct{}
+// Define the handler used by gin middleware
+func postStartFunc() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// Instrument the http client
+		client := http.Client{
+			Transport: otelhttp.NewTransport(http.DefaultTransport),
+		}
+		re, err := http.NewRequestWithContext(
+			// Pass request context rather than gin context
+			ctx.Request.Context(),
+			"GET",
+			"https://pokeapi.co/api/v2/pokedex/kanto/",
+			nil,
+		)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		response, err := client.Do(re)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		// Write response to page
+		ctx.Writer.Write(responseData)
+		// fmt.Println(string(responseData))
 
-func (t temp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// HTTP transport to instrument http.Client:
-	client := http.Client{
-		Transport: otelhttp.NewTransport(http.DefaultTransport),
+		// Execute the pending handlers in the chain
+		ctx.Next()
 	}
-	ctx := r.Context()
-	span := trace.SpanFromContext(r.Context())
-	span.SetAttributes(
-		attribute.String("foo", "bar"),
-	)
-	//
-	re, err := http.NewRequestWithContext(
-		ctx,
-		"GET",
-		"http://pokeapi.co/api/v2/pokedex/kanto/",
-		nil,
-	)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	response, err := client.Do(re)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	fmt.Fprint(w, string(responseData))
-}
-
-// Wrap the HTTP handler func with OTel HTTP instrumentation
-func wrapHandler(f http.Handler, fname string) http.Handler {
-	wrappedHandler := otelhttp.NewHandler(f, fname)
-	http.Handle("/", wrappedHandler)
-	return wrappedHandler
 }
 
 func main() {
@@ -104,6 +106,7 @@ func main() {
 
 	engine := gin.New()
 	engine.Use(otelgin.Middleware("Go-service-middleware"))
-	engine.GET("/", gin.WrapH(wrapHandler(temp{}, "getPokedex")))
+	health := engine.Group("/health")
+	health.GET("/v2", postStartFunc())
 	engine.Run()
 }
